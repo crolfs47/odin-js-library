f23e9863a19c88b64f28ca1c9a178a70088e7590
Cathy Rolfs <crolfs47@gmail.com>
2023-04-13 11:00:32 -0500

    Add functions to take user input and create a new book, and add book to library

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 let myLibrary = [];
-let readStatus = null;
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.info = function() {
-    if (read == true) {
-      readStatus = "read"
-    }
-    else {
-      readStatus = "not read yet"
-    }
-  }
-  // the constructor...
+const newBookForm = document.querySelector('.new-book-form');
+
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
+
+function createNewBook() {
+  let newTitle = document.getElementById('newTitle');
+  let newAuthor = document.getElementById('newAuthor');
+  let newPages = document.getElementById('newPages');
+  let newRead = document.getElementById('newRead').checked;
+
+  let newBook = new Book(newTitle, newAuthor, newPages, newRead);
+  addBookToLibrary(newBook);
+}
+
+function displayBooks() {
+
+}
+
+newBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  createNewBook();
+  console.log(myLibrary);
+});
+
+// Manually add books to library
+book1 = new Book('The Great Believers', 'Rebecca Marakai', '421', true);
+addBookToLibrary(book1);
+book2 = new Book('Educated', 'Tara Westover', '352', true);
+addBookToLibrary(book2);
+book3 = new Book('Black Cake', 'Charmaine Wilkerson', '385', true);
+addBookToLibrary(book3);
+
+console.log('Test');
+console.log(myLibrary);
